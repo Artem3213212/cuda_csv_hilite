8e561211ecd047de0ef1ef645fb01a0b1d2d55e6
Alexey-t <support@uvviewsoft.com>
2018-06-10 21:17:47 +0300

    tabs to spaces

--- csv_proc.py
+++ csv_proc.py
@@ -7,33 +7,33 @@ def parse_csv_line(s,sep=',',quote='"'):
     res=[]
     col,x,b=0,0,True
     for i,c in enumerate(s):
-	if c==sep and b:
-	    if x!=i:
-		res.append([x,i,col])
-		res.append([i,i+1,-1])
-	    else:
-		if i!=0:
-		    res[-1][1]+=1
-		else:
-		    res.append([0,1,-1])
-	    x=i+1
-	    col+=1
-	elif c==quote:
-	    b=not b
+        if c==sep and b:
+            if x!=i:
+                res.append([x,i,col])
+                res.append([i,i+1,-1])
+            else:
+                if i!=0:
+                    res[-1][1]+=1
+                else:
+                    res.append([0,1,-1])
+            x=i+1
+            col+=1
+        elif c==quote:
+            b=not b
     if x!=len(s):
-	res.append([x,len(s),col])
+        res.append([x,len(s),col])
     if not b:
-	return []
+        return []
     s=s.replace(quote*2,'')
     i=-1
     while True:
-	i=s.find(quote,i+1)
-	if i==-1:
-	    return res
-	if not(i==0 or s[i-1]==sep):
-	    break
-	i=s.find(quote,i+1)
-	if not(i==len(s)-1 or s[i+1]==sep):
-	    break
+        i=s.find(quote,i+1)
+        if i==-1:
+            return res
+        if not(i==0 or s[i-1]==sep):
+            break
+        i=s.find(quote,i+1)
+        if not(i==len(s)-1 or s[i+1]==sep):
+            break
     return []
 
