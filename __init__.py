df51536088d331ccef2540c4acd696b3cf5123f1
Alexey-t <support@uvviewsoft.com>
2018-06-10 21:13:31 +0300

    make file csv_proc

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import os
 from cudatext import *
+from .csv_proc import *
 
 fn_config = os.path.join(app_path(APP_DIR_SETTINGS), 'cuda_csv_hilite.ini')
 
@@ -8,40 +9,6 @@ option_bool = True
 
 def bool_to_str(v): return '1' if v else '0'
 def str_to_bool(s): return s=='1'
-
-def parse_csv_line(s,sep=',',quote='"'):
-    res=[]
-    col,x,b=0,0,True
-    for i,c in enumerate(s):
-	if c==sep and b:
-	    if x!=i:
-		res.append([x,i,col])
-		res.append([i,i+1,-1])
-	    else:
-		if i!=0:
-		    res[-1][1]+=1
-		else:
-		    res.append([0,1,-1])
-	    x=i+1
-	    col+=1
-	elif c==quote:
-	    b=not b
-    if x!=len(s):
-	res.append([x,len(s),col])
-    if not b:
-	return []
-    s=s.replace(quote*2,'')
-    i=-1
-    while True:
-	i=s.find(quote,i+1)
-	if i==-1:
-	    return res
-	if not(i==0 or s[i-1]==sep):
-	    break
-	i=s.find(quote,i+1)
-	if not(i==len(s)-1 or s[i+1]==sep):
-	    break
-    return []
 
 class Command:
     
