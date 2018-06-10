e942abc13c615898c7097602e2641ad8c076eb68
Alexey-t <support@uvviewsoft.com>
2018-06-10 21:37:32 +0300

    working

--- __init__.py
+++ __init__.py
@@ -3,6 +3,9 @@ from cudatext import *
 from .csv_proc import *
 
 fn_config = os.path.join(app_path(APP_DIR_SETTINGS), 'cuda_csv_hilite.ini')
+MYTAG = 201
+PALETTE = (0xA00000,0x00A000,0x0000A0,0xAAAA00,0x00AAAA,0xAA00AA,0x80FF80)
+COLOR_COMMA = 0x000080
 
 option_int = 100
 option_bool = True
@@ -25,11 +28,38 @@ class Command:
         ini_write(fn_config, 'op', 'option_bool', bool_to_str(option_bool))
         file_open(fn_config)
         
-    def run(self):
-        pass
-
     def on_open(self, ed_self):
-        pass
+    
+        self.update()
 
     def on_scroll(self, ed_self):
-        pass
+    
+        self.update()
+
+    def update(self):
+    
+        if ed.get_prop(PROP_LEXER_FILE, '') != 'CSV': return
+        ed.attr(MARKERS_DELETE_BY_TAG, tag=MYTAG)
+    
+        line1 = ed.get_prop(PROP_LINE_TOP)
+        line2 = ed.get_prop(PROP_LINE_BOTTOM)+1
+        
+        for line in range(line1, line2+1):
+            s = ed.get_text_line(line)
+            res = parse_csv_line(s)
+            if not res: continue
+            for x1, x2, kind in res:
+            
+                if kind<0:
+                    ncolor = COLOR_COMMA
+                else:
+                    ncolor = PALETTE[kind%len(PALETTE)]
+                    
+                ed.attr(MARKERS_ADD, 
+                    tag=MYTAG,
+                    x=x1,
+                    y=line, 
+                    len=x2-x1,
+                    color_font=ncolor,
+                    color_bg=COLOR_NONE,
+                    )
