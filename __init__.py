f83898c2363b56d5a773cf336cb3045b0d9252ae
Alexey-t <support@uvviewsoft.com>
2018-06-10 23:43:10 +0300

    del old repaint

--- __init__.py
+++ __init__.py
@@ -1,19 +1,19 @@
 import os
 from cudatext import *
-import cudatext_cmd as cmds
+#import cudatext_cmd as cmds
 from .csv_proc import *
 
 fn_config = os.path.join(app_path(APP_DIR_SETTINGS), 'cuda_csv_hilite.ini')
 MYTAG = 201
+TIMERFUNC = 'module=cuda_csv_hilite;cmd=timer_tick;'
 
-if hasattr(cmds, 'cmd_RepaintEditor'):
-    REPAINT_CMD = cmds.cmd_RepaintEditor
-else:
-    REPAINT_CMD = -1
+#if hasattr(cmds, 'cmd_RepaintEditor'):
+#    REPAINT_CMD = cmds.cmd_RepaintEditor
+#else:
+#    REPAINT_CMD = -1
 
 PALETTE = (0xFF0000,0x00AA00,0x0000FF,0x880000,0x004400,0x000088)
 COLOR_COMMA = 0x000000
-TIMERFUNC = 'module=cuda_csv_hilite;cmd=timer_tick;'
 
 option_int = 100
 option_bool = True
@@ -85,5 +85,3 @@ class Command:
                     color_bg=COLOR_NONE,
                     )
 
-        if REPAINT_CMD>0:
-            ed.cmd(REPAINT_CMD)
