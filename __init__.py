5dc4009f4eef484dfc4f8b71486c33761c3b78e1
Alexey-t <support@uvviewsoft.com>
2018-06-10 21:16:13 +0300

    tabs to spaces

--- __init__.py
+++ __init__.py
@@ -14,30 +14,22 @@ class Command:
     
     def __init__(self):
 
-	global option_int
-	global option_bool
-	option_int = int(ini_read(fn_config, 'op', 'option_int', str(option_int)))
-	option_bool = str_to_bool(ini_read(fn_config, 'op', 'option_bool', bool_to_str(option_bool)))
+        global option_int
+        global option_bool
+        option_int = int(ini_read(fn_config, 'op', 'option_int', str(option_int)))
+        option_bool = str_to_bool(ini_read(fn_config, 'op', 'option_bool', bool_to_str(option_bool)))
 
     def config(self):
 
-	ini_write(fn_config, 'op', 'option_int', str(option_int))
-	ini_write(fn_config, 'op', 'option_bool', bool_to_str(option_bool))
-	file_open(fn_config)
-	
+        ini_write(fn_config, 'op', 'option_int', str(option_int))
+        ini_write(fn_config, 'op', 'option_bool', bool_to_str(option_bool))
+        file_open(fn_config)
+        
     def run(self):
-	s = '''
-	file lines count: {cnt}
-	option_int: {i}
-	option_bool: {b}
-	'''.format(
-	     cnt = ed.get_line_count(),
-	     i = option_int,
-	     b = option_bool,
-	     )
-	msg_box(s, MB_OK)
+        pass
 
     def on_open(self, ed_self):
-	pass
+        pass
+
     def on_scroll(self, ed_self):
-	pass
+        pass
