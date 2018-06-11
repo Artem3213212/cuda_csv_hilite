460789f28c76653fa87d460dfe7c4492f1b7349c
Alexey-t <support@uvviewsoft.com>
2018-06-11 10:24:18 +0300

    trim sp

--- __init__.py
+++ __init__.py
@@ -25,16 +25,16 @@ def _getitem(name):
     for i in _theme:
         if i['name']==name:
             return i['color_font']
-            
+
 def get_theme_palette():
     return [_getitem(s) for s in option_colors_themed.split(',')]
-    
+
 def get_theme_comma():
     return _getitem('Symbol')
-    
+
 
 class Command:
-    
+
     def __init__(self):
 
         global option_color_comma
@@ -43,18 +43,18 @@ class Command:
         global option_use_theme_colors
         global PALETTE
         global COLOR_COMMA
-        
+
         option_color_comma = ini_read(fn_config, 'op', 'color_comma', option_color_comma)
         option_colors_fixed = ini_read(fn_config, 'op', 'colors_fixed', option_colors_fixed)
         option_colors_themed = ini_read(fn_config, 'op', 'colors_themed', option_colors_themed)
         option_use_theme_colors = str_to_bool(ini_read(fn_config, 'op', 'use_theme_colors', bool_to_str(option_use_theme_colors)))
-        
+
         if option_use_theme_colors:
             COLOR_COMMA = get_theme_comma()
             PALETTE = get_theme_palette()
         else:
             COLOR_COMMA = html_color_to_int(option_color_comma)
-            PALETTE = [html_color_to_int(s) for s in option_colors_fixed.split(',')] 
+            PALETTE = [html_color_to_int(s) for s in option_colors_fixed.split(',')]
 
     def config(self):
 
@@ -63,51 +63,51 @@ class Command:
         ini_write(fn_config, 'op', 'colors_themed', option_colors_themed)
         ini_write(fn_config, 'op', 'use_theme_colors', bool_to_str(option_use_theme_colors))
         file_open(fn_config)
-        
+
     def on_open(self, ed_self):
-    
+
         self.update()
 
     def on_scroll(self, ed_self):
-    
+
         self.update()
 
     def on_change_slow(self, ed_self):
-    
+
         self.update()
 
     def update(self):
-    
+
         timer_proc(TIMER_STOP, TIMERCALL, 0)
         timer_proc(TIMER_START_ONE, TIMERCALL, TIMERTIME)
-        
+
     def timer_tick(self, tag='', info=''):
-    
+
         self.update_work()
-    
+
     def update_work(self):
-    
+
         ed.attr(MARKERS_DELETE_BY_TAG, tag=MYTAG)
         line1 = ed.get_prop(PROP_LINE_TOP)
         line2 = ed.get_prop(PROP_LINE_BOTTOM)
-        
+
         for line in range(line1, line2+1):
             s = ed.get_text_line(line)
             if not s: continue
-            
+
             res = parse_csv_line(s)
             if not res: continue
-            
+
             for x1, x2, kind in res:
                 if kind<0:
                     ncolor = COLOR_COMMA
                 else:
                     ncolor = PALETTE[kind%len(PALETTE)]
-                    
-                ed.attr(MARKERS_ADD, 
+
+                ed.attr(MARKERS_ADD,
                     tag=MYTAG,
                     x=x1,
-                    y=line, 
+                    y=line,
                     len=x2-x1,
                     color_font=ncolor,
                     color_bg=COLOR_NONE,
@@ -115,34 +115,34 @@ class Command:
 
 
     def get_header(self, n):
-        
+
         s = ed.get_text_line(0)
         if not s: return
         res = parse_csv_line(s)
         if not res: return
-        
+
         for x1, x2, kind in res:
             if kind==n:
                 s = s[x1:x2]
                 s = s.strip('"').replace('""', '"')
                 return s
-                
+
 
     def on_mouse_stop(self, ed_self, x, y):
-    
+
         msg_status('')
         res = ed_self.convert(CONVERT_PIXELS_TO_CARET, x, y, '')
         if res is None: return
         x, y = res
         if y==0: return
-        
+
         s = ed.get_text_line(y)
         if not s: return
         if x>=len(s): return
-        
+
         res = parse_csv_line(s)
         if not res: return
-        
+
         for x1, x2, kind in res:
             if x1<=x<x2:
                 if kind>=0:
